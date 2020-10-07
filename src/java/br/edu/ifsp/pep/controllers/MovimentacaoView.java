44934cb88f563a73b38a34ab543a0d9ee1c0d80f
phaalonso <phaalonso@gmail.com>
2020-10-06 23:53:13 -0300

    Geração de gráfico e mudanças no css

--- src/java/br/edu/ifsp/pep/controllers/MovimentacaoView.java
+++ src/java/br/edu/ifsp/pep/controllers/MovimentacaoView.java
@@ -7,21 +7,30 @@ package br.edu.ifsp.pep.controllers;
 
 import br.edu.ifsp.pep.DAO.MovimentacaoDAO;
 import br.edu.ifsp.pep.model.Movimentacao;
+import br.edu.ifsp.pep.model.TipoMovimentacao;
 import java.io.Serializable;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
-import javax.faces.view.ViewScoped;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.BarChartSeries;
+import org.primefaces.model.chart.LegendPlacement;
+import org.primefaces.model.chart.LineChartModel;
+import org.primefaces.model.chart.LineChartSeries;
 
 /**
  *
  * @author pedro
  */
 @Named("movimentacaoView")
-@ViewScoped
+@RequestScoped
 public class MovimentacaoView implements Serializable {
 	@EJB
 	private	MovimentacaoDAO movimentacaoControllerAO;
+
+	private LineChartModel model;
 
 	private List<Movimentacao> itens;
 
@@ -35,5 +44,35 @@ public class MovimentacaoView implements Serializable {
 
 	public void setMovimentacaoControllerAO(MovimentacaoDAO movimentacaoControllerAO) {
 		this.movimentacaoControllerAO = movimentacaoControllerAO;
+	}
+
+	public LineChartModel getModel(Integer idContaOrigem) {
+		this.model = new LineChartModel();
+
+		model.setTitle("Valores de transações e seus tipos");
+		model.setLegendPosition("e");
+		model.setLegendPlacement(LegendPlacement.OUTSIDE);
+		
+		List<Movimentacao> movimentacoes = movimentacaoControllerAO.selectByContaOrigem(idContaOrigem);
+
+		LineChartSeries depositosS = new LineChartSeries();
+		LineChartSeries saqueS = new LineChartSeries();
+		LineChartSeries transferenciaS = new LineChartSeries();
+
+		depositosS.setLabel("Depósitos");
+		saqueS.setLabel("Saque");
+		transferenciaS.setLabel("Transferencia");
+		
+		//TODO ARRUMAR GRAFICO
+
+		model.addSeries(saqueS);
+		model.addSeries(depositosS);
+		model.addSeries(transferenciaS);
+		
+		return model;
+	}
+
+	public void setModel(LineChartModel model) {
+		this.model = model;
 	}
 }
